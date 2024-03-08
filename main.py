72baa3d9dbcfdff60b8a6fb521d4ec6c60dc3f0a
KimberlyPangilinan <kimberlypangilinan2001@gmail.com>
2024-03-08 14:46:26 +0800

    feat: univs and keywords

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from routes.check import check_bp
 from routes.journal import journal_bp
 from routes.auth import auth_bp
 from routes.faqs import faqs_bp
+from routes.universities import universities_bp
 from datetime import timedelta
 app = Flask(__name__)
 
@@ -21,5 +22,6 @@ app.register_blueprint(check_bp, url_prefix='/api/check')
 app.register_blueprint(journal_bp, url_prefix='/api/journal')
 app.register_blueprint(faqs_bp, url_prefix='/api/faqs')
 app.register_blueprint(auth_bp, url_prefix='/api/auth')
+app.register_blueprint(universities_bp, url_prefix='/api/universities')
 if __name__ == '__main__':
   app.run(port=5000)
---
+++ routes/universities.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify,Blueprint
+from db import db
+import json
+universities_bp = Blueprint('universities',__name__)
+
+# Load the JSON data from your file
+with open('./ipynb/universities.json', 'r') as file:
+    universities_data = json.load(file)
+    
+@universities_bp.route('/', methods=['GET'])
+def search_universities():
+    title = request.args.get('title', "") 
+    
+    universities = [uni for uni in universities_data if title.lower() in uni.lower()]
+    
+    results = universities
+    
+    return jsonify(results), 200
